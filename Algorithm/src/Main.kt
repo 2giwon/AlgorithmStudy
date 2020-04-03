267eb5099a8b86a9218a82404df3dfda4e09dbc9
middlefgr <2giwon@gmail.com>
2020-04-04 00:08:10 +0900

    #01 leetcode https://leetcode.com/problems/maximum-subarray/ 솔루션 추가

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -1,9 +1,25 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    val result = Solution().maxSubArray(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4))
+    println(result)
 }
+
+// [-2,1,-3,4,-1,2,1,-5,4]
+fun IntArray.sumSubArray(): Int = this.fold(0) { a, b -> a + b }
 
 class Solution {
     fun maxSubArray(nums: IntArray): Int {
-        return 0
+
+        var maxNum = nums[0]
+        for (i in nums.indices) {
+            for (j in i..nums.lastIndex) {
+                val foldResult = nums.sliceArray(i..j)
+                        .sumSubArray()
+
+                if (maxNum < foldResult) maxNum = foldResult
+
+            }
+        }
+        return maxNum
     }
 }
