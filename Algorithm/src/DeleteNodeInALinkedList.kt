7b9efc546b8fae50c7729c46d7b884ce4955e1e3
2giwon <2giwon@gmail.com>
2020-06-04 10:07:38 +0900

    #002 30일 챌린지 2번 문제 delete node in a linked list 솔루션 https://leetcode.com/explore/featured/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3348/

--- Algorithm/src/DeleteNodeInALinkedList.kt
+++ Algorithm/src/DeleteNodeInALinkedList.kt
@@ -1,1 +1,14 @@
-class DeleteNodeInALinkedList
+class DeleteNodeInALinkedList {
+    val linkedList = ListNode(4).apply {
+        next = ListNode(5).apply {
+            next = ListNode(1).apply {
+                next = ListNode(9)
+            }
+        }
+    }
+
+    fun deleteNode(node: ListNode?) {
+        node?.v = node?.next?.v
+        node?.next = node?.next?.next
+    }
+}
