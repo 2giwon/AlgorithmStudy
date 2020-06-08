585d7039b6a4e925c3706daee48e2bf7441ea8ba
2giwon <2giwon@gmail.com>
2020-06-08 09:53:49 +0900

    #005 30일 챌린지 5번 문제 Random Pick with Weight 솔루션(대체 뭔말인지 모르겠다) https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3351/

--- Algorithm/src/RandomPickWIthWeight.kt
+++ Algorithm/src/RandomPickWIthWeight.kt
@@ -1,5 +1,19 @@
-class RandomPickWIthWeight(w: IntArray) {
-    fun pickIndex(): Int {
-        return 0
+import kotlin.random.Random
+
+class RandomPickWIthWeight(private val w: IntArray) {
+    private val max = w.sum()
+    private val random = Random.Default
+
+    fun pickIndex(): Int = random.nextInt(max).let { random ->
+        // accIndex is the index of the result
+        // accValue is the sum of the value in the array until the value is found.
+        w.foldIndexed(0 to 0) { index, (accValue, accIndex), value ->
+            when {
+                // if random falls into this part of the pie
+                random in accValue until accValue + value -> accValue + value to index
+                random >= accValue + value -> accValue + value to accIndex
+                else -> accValue to accIndex
+            }
+        }.second
     }
 }
