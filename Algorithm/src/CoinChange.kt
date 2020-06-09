a6bfb137fbd427f92d49b5cbea06b0a58c41c3bd
2giwon <2giwon@gmail.com>
2020-06-09 14:27:54 +0900

    #007 30일 챌린지 7번 문제 Coin Change 솔루션 https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3353/

--- Algorithm/src/CoinChange.kt
+++ Algorithm/src/CoinChange.kt
@@ -1,5 +1,17 @@
 class CoinChange {
     fun change(amount: Int, coins: IntArray): Int {
-        return 0
+        val dp = Array(coins.size + 1) { IntArray(amount + 1) }
+        dp[0][0] = 1
+
+        for (i in 1..coins.size) {
+            dp[i][0] = 1
+            for (j in 1..amount) {
+                dp[i][j] = dp[i - 1][j] + if (j >= coins[i - 1]) dp[i][j - coins[i - 1]] else 0
+            }
+        }
+
+        return dp[coins.size][amount]
     }
+
+
 }
