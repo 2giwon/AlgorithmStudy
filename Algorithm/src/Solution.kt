b22ea3ff3467d500da4a7f5c9dac9c9fb73de210
middlefgr <2giwon@gmail.com>
2020-06-13 18:38:23 +0900

    #016 Insert Delete GetRandom O(1) 문제 추가 https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3358/

--- Algorithm/src/Solution.kt
+++ Algorithm/src/Solution.kt
@@ -306,4 +306,38 @@ object Solution {
     private fun sortColors() {
         println(SortColors().sortColors(intArrayOf(2, 0, 2, 1, 1, 0)).toList())
     }
+
+    private fun randomizedSet() {
+        // Init an empty set.
+        // Init an empty set.
+        val randomSet = RandomizedSet()
+
+        // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+        // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+        randomSet.insert(1)
+
+        // Returns false as 2 does not exist in the set.
+        // Returns false as 2 does not exist in the set.
+        randomSet.remove(2)
+
+        // Inserts 2 to the set, returns true. Set now contains [1,2].
+        // Inserts 2 to the set, returns true. Set now contains [1,2].
+        randomSet.insert(2)
+
+        // getRandom should return either 1 or 2 randomly.
+        // getRandom should return either 1 or 2 randomly.
+        randomSet.getRandom()
+
+        // Removes 1 from the set, returns true. Set now contains [2].
+        // Removes 1 from the set, returns true. Set now contains [2].
+        randomSet.remove(1)
+
+        // 2 was already in the set, so return false.
+        // 2 was already in the set, so return false.
+        randomSet.insert(2)
+
+        // Since 2 is the only number in the set, getRandom always return 2.
+        // Since 2 is the only number in the set, getRandom always return 2.
+        randomSet.getRandom()
+    }
 }
---
+++ Algorithm/src/leetcode/challenge/june/RandomizedSet.kt
@@ -0,0 +1,29 @@
+package leetcode.challenge.june
+
+class RandomizedSet {
+    /** Initialize your data structure here. */
+
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    fun insert(`val`: Int): Boolean {
+        return true
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    fun remove(`val`: Int): Boolean {
+        return true
+    }
+
+    /** Get a random element from the set. */
+    fun getRandom(): Int {
+        return 0
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = RandomizedSet()
+ * var param_1 = obj.insert(`val`)
+ * var param_2 = obj.remove(`val`)
+ * var param_3 = obj.getRandom()
+ */
