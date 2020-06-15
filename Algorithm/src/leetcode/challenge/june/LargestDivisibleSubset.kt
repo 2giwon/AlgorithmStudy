00d75981155dc53f9c88082747f18975da4c6202
2giwon <2giwon@gmail.com>
2020-06-15 11:01:34 +0900

    #016 30일 챌린지 13 문제 Largest Divisible Subset 솔루션 https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3359/

---
+++ Algorithm/src/leetcode/challenge/june/LargestDivisibleSubset.kt
@@ -0,0 +1,38 @@
+package leetcode.challenge.june
+
+class LargestDivisibleSubset {
+    fun largestDivisibleSubset(nums: IntArray): List<Int> {
+        val result = mutableSetOf<List<Int>>()
+        val subList = mutableSetOf<Int>()
+        var isInsert = true
+        nums.sort()
+
+        for (i in nums.indices) {
+            subList.clear()
+            for (j in nums.indices) {
+                if (nums[i] % nums[j] == 0 || nums[j] % nums[i] == 0) {
+                    if (subList.size > 0) {
+                        for (k in subList.indices) {
+                            if (subList.elementAt(k) % nums[j] != 0 && nums[j] % subList.elementAt(k) != 0) {
+                                isInsert = false
+                            }
+                        }
+
+                        if (isInsert) {
+                            subList.add(nums[j])
+                        }
+                    } else {
+                        subList.add(nums[i])
+                        subList.add(nums[j])
+                    }
+
+                    isInsert = true
+                }
+            }
+
+            result.add(subList.toList())
+        }
+
+        return result.maxBy { it.size }?.sorted() ?: emptyList()
+    }
+}
