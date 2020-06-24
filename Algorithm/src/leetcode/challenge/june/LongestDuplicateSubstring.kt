99b48e37c88f0f364b3383db0eef7f894b2fd4d8
2giwon <2giwon@gmail.com>
2020-06-24 21:30:24 +0900

    #022 30일 챌린지 19 문제 Longest Duplicate Substring 문제 푸는중 https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/541/week-3-june-15th-june-21st/3365/

--- Algorithm/src/leetcode/challenge/june/LongestDuplicateSubstring.kt
+++ Algorithm/src/leetcode/challenge/june/LongestDuplicateSubstring.kt
@@ -2,6 +2,16 @@ package leetcode.challenge.june
 
 class LongestDuplicateSubstring {
     fun longestDupSubstring(S: String): String {
-        return ""
+        var findString = ""
+        val mid = S.length / 2
+        val result = ""
+        for (i in mid downTo 1) {
+            for (j in 0..(findString.length - i)) {
+                findString = S.substring(j, j + i)
+
+            }
+        }
+
+        return result
     }
 }
