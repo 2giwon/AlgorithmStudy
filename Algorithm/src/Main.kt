51aa2103be927cc10584a84640615c3b7efe7f23
2giwon <2giwon@infrawareglobal.com>
2020-09-17 20:04:43 +0900

    #023 leetCode https://leetcode.com/problems/word-pattern/

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -1,4 +1,10 @@
+import leetcode.study.Solution
 
 fun main(args: Array<String>) {
-    Solution.solution()
+//    Solution.solution()
+    println(Solution().wordPattern("abba", "dog cat cat dog"))
+    println(Solution().wordPattern("abba", "dog cat cat fish"))
+    println(Solution().wordPattern("aaaa", "dog cat cat dog"))
+    println(Solution().wordPattern("abba", "dog dog dog dog"))
+    println(Solution().wordPattern("aaa", "aa aa aa aa"))
 }
