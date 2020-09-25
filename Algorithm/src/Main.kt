a29960732d27c0e4b03454b9cd88a122053687e1
2giwon <2giwon@gmail.com>
2020-09-25 13:47:35 +0900

    #024 https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
    풀이

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -1,10 +1,8 @@
-import leetcode.study.Solution
+import leetcode.study.TapeEquilibrium
 
 fun main(args: Array<String>) {
 //    Solution.solution()
-    println(Solution().wordPattern("abba", "dog cat cat dog"))
-    println(Solution().wordPattern("abba", "dog cat cat fish"))
-    println(Solution().wordPattern("aaaa", "dog cat cat dog"))
-    println(Solution().wordPattern("abba", "dog dog dog dog"))
-    println(Solution().wordPattern("aaa", "aa aa aa aa"))
+    println(TapeEquilibrium().tapeEquilibrium(intArrayOf(3, 1, 2, 4, 3)))
+    println(TapeEquilibrium().tapeEquilibrium(intArrayOf(-1000, 1000)))
+    println(TapeEquilibrium().tapeEquilibrium(intArrayOf(-2, -3, -4, -1)))
 }
