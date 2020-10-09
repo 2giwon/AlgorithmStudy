3b7a4f57ee502f08b1ab25766fb23d514e7f6dd7
middlefgr <2giwon@gmail.com>
2020-10-09 22:30:06 +0900

    #25 프로그래머스 문제 풀기 타켓넘버 완료 https://programmers.co.kr/learn/courses/30/lessons/43165?language=kotlin

--- Algorithm/src/leetcode/study/TargetNumber.kt
+++ Algorithm/src/leetcode/study/TargetNumber.kt
@@ -1,12 +1,22 @@
 package leetcode.study
 
 class TargetNumber {
+    private var target = 0
     fun solution(numbers: IntArray, target: Int): Int {
-        var answer = 0
-        return answer
+        this.target = target
+        return reqCal(numbers, 0, 0, 0)
     }
 
-//    private fun reqCal(numbers: IntArray, current: Int, target: Int): Int {
-//
-//    }
+    private fun reqCal(numbers: IntArray, i: Int, acc: Int, ans: Int): Int {
+        if (acc == target && i == numbers.size) return ans + 1
+        if (i >= numbers.size) return ans
+
+        var current = acc + numbers[i]
+        var answer = ans + reqCal(numbers, i + 1, current, ans)
+
+        current = acc + (-numbers[i])
+        answer += reqCal(numbers, i + 1, current, ans)
+
+        return answer
+    }
 }
