ae57618049ebc22d196f128448deff8f9ad52572
middlefgr <2giwon@gmail.com>
2020-10-09 21:22:14 +0900

    #25 프로그래머스 문제 풀기 타켓넘버 https://programmers.co.kr/learn/courses/30/lessons/43165?language=kotlin

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -1,8 +1,6 @@
-import leetcode.study.TapeEquilibrium
+import leetcode.study.TargetNumber
 
 fun main(args: Array<String>) {
 //    Solution.solution()
-    println(TapeEquilibrium().tapeEquilibrium(intArrayOf(3, 1, 2, 4, 3)))
-    println(TapeEquilibrium().tapeEquilibrium(intArrayOf(-1000, 1000)))
-    println(TapeEquilibrium().tapeEquilibrium(intArrayOf(-2, -3, -4, -1)))
+    println(TargetNumber().solution(intArrayOf(1, 1, 1, 1, 1), 3))
 }
---
+++ Algorithm/src/leetcode/study/TargetNumber.kt
@@ -0,0 +1,12 @@
+package leetcode.study
+
+class TargetNumber {
+    fun solution(numbers: IntArray, target: Int): Int {
+        var answer = 0
+        return answer
+    }
+
+//    private fun reqCal(numbers: IntArray, current: Int, target: Int): Int {
+//
+//    }
+}
