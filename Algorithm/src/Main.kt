12c1c3671937f2ce90f816ac49ef1a807092345a
2giwon <2giwon@gmail.com>
2020-10-16 16:06:27 +0900

    #026 CombinationSum 문제 와 풀이 https://leetcode.com/problems/combination-sum/submissions/

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -1,6 +1,8 @@
-import leetcode.study.TargetNumber
+import leetcode.study.CombinationSum
 
 fun main(args: Array<String>) {
 //    Solution.solution()
-    println(TargetNumber().solution(intArrayOf(1, 1, 1, 1, 1), 3))
+    println(CombinationSum().combinationSum(intArrayOf(2, 3, 6, 7), 7))
+    println(CombinationSum().combinationSum(intArrayOf(2, 3, 5), 8))
+    println(CombinationSum().combinationSum(intArrayOf(2), 1))
 }
