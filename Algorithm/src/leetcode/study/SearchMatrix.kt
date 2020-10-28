f86f046a788dccb0f20118adccee70a115570448
2giwon <2giwon@gmail.com>
2020-10-28 12:36:13 +0900

    #028 search matrix 2d 솔루션 https://leetcode.com/problems/search-a-2d-matrix/

--- Algorithm/src/leetcode/study/SearchMatrix.kt
+++ Algorithm/src/leetcode/study/SearchMatrix.kt
@@ -2,6 +2,15 @@ package leetcode.study
 
 class SearchMatrix {
     fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+        for (m in matrix.iterator()) {
+            if (m.isNotEmpty() && m.first() <= target && m.last() >= target) {
+                for (e in m) {
+                    if (e == target) return true
+                    else if (e > target) return false
+                }
+            }
+        }
+
         return false
     }
 }
