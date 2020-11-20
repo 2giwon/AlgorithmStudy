d3de0c868d90ddfbce367c78dcd66cb1ecfa2eae
2giwon <2giwon@gmail.com>
2020-11-20 11:16:58 +0900

    #031 Permutations II 문제 및 2차 솔루션(속도 업) https://leetcode.com/problems/permutations-ii/

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -1,7 +1,7 @@
 import leetcode.study.Permutations
 
 fun main(args: Array<String>) {
-//    println(Permutations().permuteUnique(intArrayOf(1, 1, 2)))
+    println(Permutations().permuteUnique(intArrayOf(1, 1, 2)))
 //    println(Permutations().permuteUnique(intArrayOf(1, 2, 3)))
 //    println(Permutations().permuteUnique(intArrayOf(1)))
     println(Permutations().permuteUnique(intArrayOf(0, 1, 0, 0, 9)))
--- Algorithm/src/leetcode/study/Permutations.kt
+++ Algorithm/src/leetcode/study/Permutations.kt
@@ -1,66 +1,40 @@
 package leetcode.study
 
 class Permutations {
-
-//    fun permuteUnique(nums: IntArray): List<List<Int>> {
-//        // input check, the length of nums >= 1
-//        val res: MutableList<List<Int>> = ArrayList()
-//
-//        helper(nums, 0, res)
-//        return res
-//    }
-//
-//    private fun helper(nums: IntArray, index: Int, res: MutableList<List<Int>>) {
-//        // base case
-//        if (index >= nums.size) {
-//            val list: MutableList<Int> = ArrayList()
-//            for (n in nums) {
-//                list.add(n)
-//            }
-//            res.add(list)
-//            return
-//        }
-//
-//        val visited = mutableSetOf<Int>()
-//        for (i in index until nums.size) {
-//            if (visited.contains(nums[i])) continue
-//
-//            visited.add(nums[i])
-//            // swap i, index
-//            swap(nums, i, index)
-//            helper(nums, index + 1, res)
-//            swap(nums, index, i)
-//        }
-//    }
-
-//    private fun swap(nums: IntArray, i: Int, j: Int) {
-//        val temp = nums[i]
-//        nums[i] = nums[j]
-//        nums[j] = temp
-//    }
-
+    private var max: Int = 0
     fun permuteUnique(nums: IntArray): List<List<Int>> {
         val result: MutableList<MutableList<Int>> = mutableListOf()
         val mutableNums = nums.toMutableList()
+        max = nums.size
 
         if (nums.size <= 1) result.add(mutableNums)
-        else req(mutableNums, nums.size, mutableListOf(), result)
+        else req(mutableNums, mutableListOf(), mutableSetOf(), 0, result)
 
-        return result.toList()
+        return result
     }
 
-    private fun req(nums: MutableList<Int>, max: Int, values: MutableList<Int>, result: MutableList<MutableList<Int>>) {
+    private fun req(
+            nums: MutableList<Int>,
+            values: MutableList<Int>,
+            visited: MutableSet<Int>,
+            depth: Int,
+            result: MutableList<MutableList<Int>>
+    ) {
         if (nums.size <= 1) {
             values.add(nums[0])
             return
         }
 
         for (i: Int in nums.indices) {
+            if (depth == 0 && visited.contains(nums[i])) continue
             val v = values.toMutableList()
             val nums2 = nums.toMutableList()
+
             v.add(nums2[i])
+            if (depth == 0) visited.add(nums2[i])
             nums2.remove(nums2[i])
-            req(nums2, max, v, result)
+            req(nums2, v, visited, depth + 1, result)
+
             if (v.size == max) result.add(v)
         }
     }
