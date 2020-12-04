4dd5f6034ed1f6082aa182724af7a5471c06639b
2giwon <2giwon@gmail.com>
2020-12-04 14:54:43 +0900

    #037 DecodeString https://leetcode.com/problems/decode-string/

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -1,5 +1,4 @@
-import leetcode.challenge.december.MaxDepthBinaryTree
-import leetcode.study.TreeNode
+import leetcode.study.DecodeString
 
 fun main(args: Array<String>) {
 //    println(RangeSumBST().rangeSumBST(TreeNode(10).apply {
@@ -23,18 +22,23 @@ fun main(args: Array<String>) {
 //        }
 //    }, 6, 10))
 
-    println(MaxDepthBinaryTree().maxDepth(TreeNode(3).apply {
-        left = TreeNode(9)
-        right = TreeNode(20).apply {
-            left = TreeNode(15)
-            right = TreeNode(7)
-        }
-    }))
+//    println(MaxDepthBinaryTree().maxDepth(TreeNode(3).apply {
+//        left = TreeNode(9)
+//        right = TreeNode(20).apply {
+//            left = TreeNode(15)
+//            right = TreeNode(7)
+//        }
+//    }))
+//
+//    println(MaxDepthBinaryTree().maxDepth(TreeNode(1).apply {
+//        right = TreeNode(2)
+//    }))
 
-    println(MaxDepthBinaryTree().maxDepth(TreeNode(1).apply {
-        right = TreeNode(2)
-    }))
+//    println(MaxDepthBinaryTree().maxDepth(null))
+//    println(MaxDepthBinaryTree().maxDepth(TreeNode(0)))
 
-    println(MaxDepthBinaryTree().maxDepth(null))
-    println(MaxDepthBinaryTree().maxDepth(TreeNode(0)))
+    println(DecodeString().decodeString("3[a]2[bc]"))
+    println(DecodeString().decodeString("3[a2[c]]"))
+    println(DecodeString().decodeString("2[abc]3[cd]ef"))
+    println(DecodeString().decodeString("abc3[cd]xyz"))
 }
