ab0fa3de40115f055694b6459ff7847dd0d3586d
2giwon <2giwon@gmail.com>
2020-12-29 14:17:07 +0900

    Swap Nodes in Pairs https://leetcode.com/problems/swap-nodes-in-pairs/

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -1,7 +1,20 @@
-import leetcode.study.Palindrome
+import leetcode.study.SwapPairs
+import util.ListNode
 
 fun main(args: Array<String>) {
 //    println(Palindrome().partition("aab"))
 //    println(Palindrome().partition("a"))
-    println(Palindrome().partition("cdd"))
+//    println(Palindrome().partition("cdd"))
+
+    val list = ListNode(1).apply {
+        next = ListNode(2).apply {
+            next = ListNode(3).apply {
+                next = ListNode(4)
+            }
+        }
+    }
+
+    println(SwapPairs().swapPairs(list))
+    println(SwapPairs().swapPairs(ListNode(null)))
+    println(SwapPairs().swapPairs(ListNode(1)))
 }
