78e853c87d7016fc017f49739352f90ed8c871c3
middlefgr <2giwon@gmail.com>
2021-01-22 23:50:06 +0900

    Minimum Operations to Reduce X to Zero 솔루션
    https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -2,5 +2,7 @@ import leetcode.study.MinOperations
 
 fun main(args: Array<String>) {
     println(MinOperations().minOperations(intArrayOf(1,1,4,2,3), 5))
+    println(MinOperations().minOperations(intArrayOf(5,6,7,8,9), 4))
     println(MinOperations().minOperations(intArrayOf(3,2,20,1,1,3), 10))
+    println(MinOperations().minOperations(intArrayOf(1,1), 3))
 }
--- Algorithm/src/leetcode/study/MinOperations.kt
+++ Algorithm/src/leetcode/study/MinOperations.kt
@@ -1,7 +1,44 @@
 package leetcode.study
 
+import kotlin.math.min
+
 class MinOperations {
     fun minOperations(nums: IntArray, x: Int): Int {
-        return 0
+        var sum = 0
+        var end = 0
+        var start = nums.lastIndex
+        var result = nums.size + 1
+
+        while(sum < x && end < nums.size) {
+            sum += nums[end++]
+        }
+
+        if (end == nums.size && sum != x) {
+            return -1
+        }
+
+        if (end == nums.size && sum == x) {
+            return nums.size
+        }
+
+        var count = end
+        while (end > -1) {
+            if (sum > x) {
+                if (end == 0) break
+                sum -= nums[(end--) - 1]
+                count--
+            } else if (sum < x) {
+                sum += nums[start--]
+                count++
+            } else {
+                result = min(result, count)
+                if (end == 0) break
+                sum -= nums[(end--) - 1]
+                count--
+            }
+        }
+
+        return if (result > nums.size) -1 else result
+
     }
 }
