d52a838b36fcb503486b86b5da5b7de872a61ba5
middlefgr <2giwon@gmail.com>
2021-01-30 00:20:08 +0900

    Find the Most Competitive Subsequence
    https://leetcode.com/problems/find-the-most-competitive-subsequence/

--- Algorithm/src/Main.kt
+++ Algorithm/src/Main.kt
@@ -1,8 +1,10 @@
-import leetcode.study.MinOperations
+import leetcode.study.MostCompetitive
 
 fun main(args: Array<String>) {
-    println(MinOperations().minOperations(intArrayOf(1,1,4,2,3), 5))
-    println(MinOperations().minOperations(intArrayOf(5,6,7,8,9), 4))
-    println(MinOperations().minOperations(intArrayOf(3,2,20,1,1,3), 10))
-    println(MinOperations().minOperations(intArrayOf(1,1), 3))
+//    println(MinOperations().minOperations(intArrayOf(1,1,4,2,3), 5))
+//    println(MinOperations().minOperations(intArrayOf(5,6,7,8,9), 4))
+//    println(MinOperations().minOperations(intArrayOf(3,2,20,1,1,3), 10))
+//    println(MinOperations().minOperations(intArrayOf(1,1), 3))
+    println(MostCompetitive().mostCompetitive(intArrayOf(3,5,2,6), 2))
+    println(MostCompetitive().mostCompetitive(intArrayOf(2,4,3,3,5,4,9,6), 4))
 }
---
+++ Algorithm/src/leetcode/study/MostCompetitive.kt
@@ -0,0 +1,7 @@
+package leetcode.study
+
+class MostCompetitive {
+    fun mostCompetitive(nums: IntArray, k: Int): IntArray {
+        return intArrayOf()
+    }
+}
