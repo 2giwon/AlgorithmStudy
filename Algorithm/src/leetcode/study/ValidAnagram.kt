6b0436b1cd532469b79fc596da570192746382d6
middlefgr <2giwon@gmail.com>
2021-02-16 22:34:14 +0900

    Valid Anagram Solution

--- Algorithm/src/leetcode/study/ValidAnagram.kt
+++ Algorithm/src/leetcode/study/ValidAnagram.kt
@@ -2,19 +2,32 @@ package leetcode.study
 
 class ValidAnagram {
     fun isAnagram(s: String, t: String): Boolean {
-        if (s.length != t.length) return false
 
-        s.forEach { c ->
-            var ret = false
-            t.forEach {
-                if (c != it) {
-                    ret = true
-                }
+        val map1 = HashMap<Char, Int>()
+        val map2 = HashMap<Char, Int>()
+
+        s.forEach {
+            if (map1[it] != null) {
+                map1[it] = (map1[it] ?: 0) + 1
+            } else {
+                map1[it] = 1
             }
-            if (!ret) return false
-            ret = false
         }
 
-        return true
+        t.forEach {
+            if (map2[it] != null) {
+                map2[it] = (map2[it] ?: 0) + 1
+            } else {
+                map2[it] = 1
+            }
+        }
+
+        t.forEach {
+            if (map1[it] == map2[it]) {
+                map1.remove(it)
+                map2.remove(it)
+            }
+        }
+        return map1.size == 0 && map2.size == 0
     }
 }
