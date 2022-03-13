a3ae4ab1e816254722fc23858cd8b031854b30fc
middlefgr <2giwon@gmail.com>
2022-03-13 10:23:52 +0900

    ContainsDuplicate2

---
+++ Algorithm/src/leetcode/grokking/ContainsDuplicate2.kt
@@ -0,0 +1,45 @@
+package leetcode.grokking
+
+//Given an integer array nums and an integer k, return true if there are two
+//distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <
+//= k.
+//
+//
+// Example 1:
+//
+//
+//Input: nums = [1,2,3,1], k = 3
+//Output: true
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [1,0,1,1], k = 1
+//Output: true
+//
+//
+// Example 3:
+//
+//
+//Input: nums = [1,2,3,1,2,3], k = 2
+//Output: false
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= nums.length <= 10⁵
+// -10⁹ <= nums[i] <= 10⁹
+// 0 <= k <= 10⁵
+//
+// Related Topics 数组 哈希表 滑动窗口 👍 454 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        return true
+    }
+}
