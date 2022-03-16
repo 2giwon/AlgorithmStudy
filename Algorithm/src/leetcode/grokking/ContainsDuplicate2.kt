a34b786f13d4c6fca0eec199a68aaf7d971d1411
middlefgr <2giwon@gmail.com>
2022-03-16 23:25:39 +0900

    [solution] - ContainsDuplicate2

--- Algorithm/src/leetcode/grokking/ContainsDuplicate2.kt
+++ Algorithm/src/leetcode/grokking/ContainsDuplicate2.kt
@@ -1,5 +1,7 @@
 package leetcode.grokking
 
+import kotlin.math.abs
+
 //Given an integer array nums and an integer k, return true if there are two
 //distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <
 //= k.
@@ -39,6 +41,16 @@ package leetcode.grokking
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        val map = mutableMapOf<Int, Int>()
+        for (i in nums.indices) {
+            if (map[nums[i]] == null) {
+                map[nums[i]] = i
+            } else {
+                val j = map[nums[i]]!!
+                if (abs(j.minus(i)) <= k) return true
+                else map[nums[i]] = i
+            }
+        }
         return false
     }
 }
