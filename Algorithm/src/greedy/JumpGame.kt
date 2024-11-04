cc263e3d1b8ee7e75a089d20eabe3418fbe2b4fe
Giwon Lee <2giwon@gmail.com>
2024-11-05 00:00:51 +0900

    솔루션 추가

--- Algorithm/src/greedy/JumpGame.kt
+++ Algorithm/src/greedy/JumpGame.kt
@@ -20,6 +20,30 @@ package greedy
     입력: nums = [3,2,1,0,4]
     출력: false
     설명: 무슨 일이 있어도 항상 인덱스 3에 도달합니다. 최대 점프 길이는 0이므로 마지막 인덱스에 도달할 수 없습니다.
+
+    솔루션
+
+    1. [1, 1, 2, 5, 2, 1, 0, 0, 1, 3]
+
+    위 배열에서 도착지점은 3이고 스타트는 [0]의 1이다
+    그런데 도착지점에 도달하려면 3 전에 1이 있다면 가능하다
+    그렇다면 lastIndex - 1 이 1 이상이어야 한다는 것이다
+    실제로 lastIndex - 1은 1이다
+
+    그렇다면 lastIndex - 1 에 도착하면 된다는 것이다
+    그럼 이제 도착지점을 lastIndex - 1로 바꾸면
+    lastIndex - 1에 도착해야 한다고 했을때 lastIndex - 2, lastIndex - 3 안된다(전부 0이므로)
+
+    lastIndex - 4 는 1인데 1증가한다고 해도 도착지점에 도달하지 못한다
+    lastIndex - 5 는 2인데 2증가한다고 해도 도착지점에 도달하지 못한다
+
+    lastIndex - 6은 5인데 5단계 점프를 하면 lastIndex - 1에 도달 할 수 있다
+    그러므로 lastIndex - 6 까지 도달하면 도착지점에 도착한다는 뜻
+
+    이런식으로 도착지점(goal)을 계속해서 도착할 수 있는 positon이 있으면 그 position으로 이동하고
+    현재 position을 한단계를 줄이면서 goal에 도착할 수 있는 지 체크하면
+    
+    선형 계수만큼 속도가 나온다.
  */
 fun main(args: Array<String>) {
     println(JumpGame().solution(nums = intArrayOf(1, 1, 2, 5, 2, 1, 0, 0, 1, 3)))
